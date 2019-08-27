64d9f648f59020727b706eb0542b832244057cd3
ぴね <eito01178@gmail.com>
2019-08-27 11:28:30 +0900

    問題ページは一通り完成

--- src/views/Questionpage.vue
+++ src/views/Questionpage.vue
@@ -6,6 +6,8 @@ div(color="#F7F3E8")
       div.headline.ml-2.mb-1 {{ time }}
       div.display-1.ml-4.font-weight-bold お題: {{ resipetitle }}
       div.body-2.mr-2.text-right {{ total_member }}人が参加しています
+      .text-right.ma-4
+        v-btn.title(color="#FFB618" @click="tovote") 投票ページへ
       
       //コンテスト材料を表示するコンポーネント
       #MatelialsArea
@@ -15,7 +17,7 @@ div(color="#F7F3E8")
         Resipe(class="ma-10 my-4 pb-10")
 
       .text-center.pt-10.pb-12
-        v-btn.title(color="#FFB618") コンテストに提出する
+        v-btn.title(color="#FFB618" @click="toanswer") コンテストに提出する
 
         //サーバーで生成したHTML埋め込み
         //iframe(
@@ -40,6 +42,14 @@ export default{
       time: "7:00 ~ 8:00",
       resipetitle:'冷やしキムチラーメン',
       total_member: 1
+    }
+  },
+  methods: {
+    tovote: function(){
+      this.$router.push("/vote")
+    },
+    toanswer: function(){
+      this.$router.push("/answer")
     }
   }
 }
--- src/views/components/Resipe.vue
+++ src/views/components/Resipe.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div
     v-card
-        v-card-title.headline.text-weight-light.text-weight-italic レシピ
+        v-card-title.headline レシピ
         ol.ml-6.pb-4
             li.body-1(v-for="line in list") {{ line.message }}
 </template>
