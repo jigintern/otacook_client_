38c1441e2fc5d43927ca8bd2a3f4f9a3adcbfde4
2ndPINEW <eito01178@gmail.com>
2019-08-29 17:35:32 +0900

    no message

--- src/views/contest/Answerpage.vue
+++ src/views/contest/Answerpage.vue
@@ -4,13 +4,13 @@ div(color="#F7F3E8")
     v-card(class="ma-4")
       //料理の名前を入れる##
       div.headline.ml-2.mb-1 {{ time }}
-      div.display-1.ml-4.font-weight-bold お題: {{ recipetitle }}
+      div.display-1.ml-4.font-weight-bold お題: {{ resipetitle }}
 
       div.headline.ma-8.mb-0 料理名
       v-text-field.ma-8.mt-0(
         :counter="30"
         value=""
-        v-model="title"
+        v-model="name"
         :rules="[rules.required, rules.namemax]"
         label="料理名を入力！"
         required)
@@ -19,7 +19,7 @@ div(color="#F7F3E8")
       v-textarea.ma-8.mt-0(
         :counter="140"
         value=""
-        v-model="comment"
+        v-model="outline"
         :rules="[rules.required, rules.commentmax]"
         label="料理について自由に書いてください！")
         required
@@ -30,8 +30,8 @@ div(color="#F7F3E8")
         label="写真を選択してください！")
 
       .text-center.pt-10.pb-12
-        //div.text-color 投票済みです
         div(v-if="isLoggingin == true")
+          div.red--text {{ errors }}
           v-btn.title(color="#FFB618" @click="toquestion") 送信
         div(v-else)
           div コンテスト参加にはログインが必要です。
@@ -41,6 +41,7 @@ div(color="#F7F3E8")
 <script>
 import Recipe from '..//components/Recipe'
 import Materials from '..//components/Materials'
+import axios from 'axios'
 
 export default{
   components: {
@@ -50,10 +51,11 @@ export default{
   data: function(){
     return{
       time: "7:00 ~ 8:00",
-      recipetitle:'冷やしキムチラーメン',
+      resipetitle:'冷やしキムチラーメン',
       total_member: 1,
-      title: "",
-      comment: "",
+      name: "",
+      outline: "",
+      errors: "",
       rules: {
         required: value => !!value || '入力必須項目です。',
         namemax: v => v.length <= 30 || '30文字以内でオナシャス',
@@ -67,7 +69,22 @@ export default{
   },
   methods: {
     toquestion: function(){
-      this.$router.push("/questionpage")
+      if(this.name=="" || this.outline==""){
+        this.errors="すべての項目を入力してから送信してください！"
+      }else{
+        const params = new URLSearchParams()
+        params.append('qi', 1)
+        params.append('ui', 3)
+        params.append('cn', this.name)
+        params.append('co', this.outline)
+        axios.post('http://localhost:8080/answer/insert', params)
+          .catch(error => {
+            console.log(error);
+            window.alert("送信失敗")
+          }).then(Response => {
+            this.$router.push('/')
+          })
+      }
     },
     tologin: function(){
       this.$router.push("/loginpage")
